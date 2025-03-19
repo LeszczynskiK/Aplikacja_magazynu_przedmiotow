2eef219b0199f96c95c5f413cd2fb4ddb4a0dc73
LeszczynskiK <leszczynskikrzysiu@gmail.com>
2025-03-19 09:56:42 +0100

    if clicked enter with no answear, you will be asked once again to type in value..

--- komendy.cpp
+++ komendy.cpp
@@ -22,8 +22,21 @@ string getString(const string &msg) // msg is value which we put in and display 
 
      do // load nazwa untill is not empty - can be any type becouse its string, but can not be empty
      {
-          cout << msg;
-          cin >> input;
+          if (cin.peek() == '\n') // this will check 1st char before data loading from cin - so it will get enter if clicked
+          {                       // if enter was clicked immediatyly (new line ) - peek will get this!
+               system("clear");
+               cout << "Wartosc nie moze byc pusta! Wprowadz ponownie." << endl;
+               cout << msg;
+               cin >> input;
+               cin.ignore();;//always ignore 1 char after cin (this char is new line char!)
+          }
+          else // not clicked enter immediately (there is something typped in)
+          {
+               cout << msg;
+               cin >> input;
+               cin.ignore();;//always ignore 1 char after cin (this char is new line char!)
+          }
+
           if (input.empty())
           {
                system("clear");
@@ -39,11 +52,24 @@ string getString(const string &msg) // msg is value which we put in and display 
 float getFloat(const string &msg) // msg is value which we put in and display here...
 {
      float value;
-     bool correct_float;
+     bool correct_float=false;
      do
      {
-          cout << msg;
-          cin >> value;
+
+          if (cin.peek() == '\n') // this will check 1st char before data loading from cin - so it will get enter if clicked
+          {                       // if enter was clicked immediatyly (new line ) - peek will get this!
+               system("clear");
+               cout << "Wartosc nie moze byc pusta! Wprowadz ponownie." << endl;
+               cout << msg;
+               cin >> value;
+               cin.ignore();//always ignore 1 char after cin (this char is new line char!)
+          }
+          else // not clicked enter immediately (there is something typped in)
+          {
+               cout << msg;
+               cin >> value;
+               cin.ignore();;//always ignore 1 char after cin (this char is new line char!)
+          }
           if (cin.fail() || value < 0) // cin.fail() - if typped other than declarated type, will return 1 so fail..
           {
                system("clear");
@@ -67,18 +93,31 @@ int getInt(const string &msg) // msg is value which we put in and display here..
      int value;
      do
      {
-          if (cin.peek() == '\n')//this will check 1st char before data loading from cin - so it will get enter if clicked
-          { // if enter was clicked immediatyly (new line ) - peek will get this!
+          if (cin.peek() == '\n') // this will check 1st char before data loading from cin - so it will get enter if clicked
+          {                       // if enter was clicked immediatyly (new line ) - peek will get this!
                system("clear");
                cout << "Wartosc nie moze byc pusta! Wprowadz ponownie." << endl;
-               first_info();
+               if (your_choice_bool) // if we are in your_choice function now, print this
+               {
+                    cout << "1. Dodawanie nowych produktów do magazynu" << endl;
+                    cout << "2. Usuwanie istniejących produktów" << endl;
+                    cout << "3. Edytowanie szczegółów produktu" << endl;
+                    cout << "4. Kopiowanie produktów" << endl;
+                    cout << "5. Wyświetlanie zawartości magazynu" << endl;
+                    cout << "6. Wyjscie z aplikacji... " << endl
+                         << endl;
+               }
                cout << msg;
                cin >> value;
+               cin.ignore();//always ignore 1 char after cin (this char is new line char!)
           }
-          else//if not enter immediately
+          else // if not enter immediately
           {
                cout << msg;
                cin >> value;
+
+               cin.ignore(); // clear 1 char from buffor (it always be \n - new line, becouse here we are after type checking)
+               // and we are sure that typped value is correct type, so we only need to delete new line char( after enter typping!)
           }
 
           if (cin.fail() || value < 0) // if <0 and not int, fail
@@ -115,8 +154,21 @@ Kategoria getCategory(const string &msg) // msg is value which we put in and dis
      int value;
      do
      {
-          cout << msg;
-          cin >> value;
+          if (cin.peek() == '\n') // this will check 1st char before data loading from cin - so it will get enter if clicked
+          {                       // if enter was clicked immediatyly (new line ) - peek will get this!
+               system("clear");
+               cout << "Wartosc nie moze byc pusta! Wprowadz ponownie." << endl;
+               cout << msg;
+               cin >> value;
+               cin.ignore();//always ignore 1 char after cin (this char is new line char!)
+          }
+          else // not clicked enter immediately (there is something typped in)
+          {
+               cout << msg;
+               cin >> value;
+               cin.ignore();//always ignore 1 char after cin (this char is new line char!)
+          }
+
           if (cin.fail() || value < 0 || value > 2)
           { // have to be int type, and have to be 0,1, or 2
                system("clear");
@@ -135,29 +187,35 @@ Kategoria getCategory(const string &msg) // msg is value which we put in and dis
 
 int your_choice()
 {
-     your_choice_bool = true; // means that we are in this funciton right now!
+     your_choice_bool = true;   // means that we are in this funciton right now!
+     int temp1;                 // number of option(option is displayed in first_info())
+     bool valid_choice = false; // it can only be 1,2,3,4,5,6
 
-     // number of option(option is displayed in first_info())
-     int temp1 = getInt("Wybor: ");
-
-     if (temp1 < 1 || temp1 > 6) // can be only 1,2,3,4,5,6 - any over/bellow this is wrong choice! int type
+     do
      {
-          // manage wrong input (get input once again - will we get correct one!)
-          system("clear");
-          cout << "Wybor niepoprawny... " << endl;
-          cout << "Wybierz jeszcze raz!" << endl
-               << endl;
+          temp1 = getInt("Wybor: ");//get user choose...
+          if (temp1 < 1 || temp1 > 6) // can be only 1,2,3,4,5,6 - any over/bellow this is wrong choice! int type
+          {
+               // manage wrong input (get input once again - will we get correct one!)
+               system("clear");
+               cout << "Wybor niepoprawny... " << endl;
+               cout << "Wybierz jeszcze raz!" << endl
+                    << endl;
 
-          cout << "1. Dodawanie nowych produktów do magazynu" << endl;
-          cout << "2. Usuwanie istniejących produktów" << endl;
-          cout << "3. Edytowanie szczegółów produktu" << endl;
-          cout << "4. Kopiowanie produktów" << endl;
-          cout << "5. Wyświetlanie zawartości magazynu" << endl;
-          cout << "6. Wyjscie z aplikacji... " << endl
-               << endl;
-          cout << "Wybor: ";
-          cin >> temp1;
-     }
+               cout << "1. Dodawanie nowych produktów do magazynu" << endl;
+               cout << "2. Usuwanie istniejących produktów" << endl;
+               cout << "3. Edytowanie szczegółów produktu" << endl;
+               cout << "4. Kopiowanie produktów" << endl;
+               cout << "5. Wyświetlanie zawartości magazynu" << endl;
+               cout << "6. Wyjscie z aplikacji... " << endl
+                    << endl;
+          }
+          else
+          {
+               valid_choice = true; // if choosed option 1,2,3,4,5 or 6 then your choose is correct
+          }
+     } while (!valid_choice); // if choice is not 1,2,3,4,5 or 6...
+
      your_choice_bool = false; // means that we are leavig this funciton right now!
      return temp1;             // operation choise is returned
 }
